02f50e4e6f36a47d52901092c70708214d936325
dxnnq <124479528+dxnnq@users.noreply.github.com>
2023-02-04 22:15:53 -0500

    simple_bot.py

---
+++ simple_bot.py
@@ -0,0 +1,29 @@
+import discord, os, datetime, json, aiosqlite, asyncio, aiohttp
+from discord.ext import commands
+
+with open("config.json") as t:
+   data = json.load(t)
+   TOKEN = data["TOKEN"]
+
+class Bot(commands.Bot):
+    def __init__(self):
+        super().__init__(
+            command_prefix = commands.when_mentioned_or("!"),
+            intents = discord.Intents.all(),
+            help_command = None #Disabling the base help command
+)
+    async def setup_hook(self):
+        print(f"Logged in as {bot.user}")
+        cogs_folder = f"{os.path.abspath(os.path.dirname(__file__))}/cogs"
+        for filename in os.listdir(cogs_folder):
+            if filename.endswith(".py"):
+                await bot.load_extension(f"cogs.{filename[:-3]}")
+        await bot.tree.sync()
+        print("Cogs Loaded")
+
+bot.run(TOKEN)
+
+#In your config.json file
+{
+    "TOKEN": "YOUR_BOT_TOKEN"
+}
