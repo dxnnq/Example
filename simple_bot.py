e3b955ac29f8ae79222bef07bd8a49edddbfec27
dxnnq <124479528+dxnnq@users.noreply.github.com>
2023-02-04 22:40:53 -0500

    Update simple_bot.py

--- simple_bot.py
+++ simple_bot.py
@@ -18,9 +18,20 @@ class Bot(commands.Bot):
         for filename in os.listdir(cogs_folder):
             if filename.endswith(".py"):
                 await bot.load_extension(f"cogs.{filename[:-3]}")
-        await bot.tree.sync()
+        await bot.tree.sync(guild=discord.Object(id=YOUR_GUILD_ID) #Remove the guild part if you want it to be global
         print("Cogs Loaded")
+         
+@bot.command()
+async def hi(ctx: commands.Context, name: str = None):
+   name = name or ctx.author.name
+   await ctx.send(f"Hello! {name}")
+   
+@bot.tree.command(description="Say hello!", guild=discord.Object(id=YOUR_GUILD_ID) #Remove the guild part if its global
+async def hello(interaction: discord.Interaction, name: str = None):
+   name = name or interaction.user.name
+   await interaction.response.send_message(f"Hello! {name}") #If you want the message to be hidden to just the user pass in an ephemeral (f"Hello!", ephemeral=True)
 
+                  
 bot.run(TOKEN)
 
 #In your config.json file
